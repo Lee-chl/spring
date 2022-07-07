20cd9112ec7f96a92f14fabf87abc9dbd93ff436
이예지 <iyeji@iyejiui-MacBookPro.local>
2022-07-07 12:21:14 +0900

    Initial commit

---
+++ src/main/java/com/example/spring/controllor/JavaExController.java
@@ -0,0 +1,11 @@
+package com.example.spring.controllor;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+@RequiredArgsConstructor
+public class JavaExController {
+}
